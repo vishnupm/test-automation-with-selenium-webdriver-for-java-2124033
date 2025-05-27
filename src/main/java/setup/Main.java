1909ca7a191ed87085199141052c9bb9709f7634
vishnupm <vishnupm87@gmail.com>
2025-05-27 11:01:19 +0000

    web driver

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -1,3 +1,5 @@
+
+
 package setup;
 
 import org.openqa.selenium.WebDriver;
@@ -8,12 +10,17 @@ import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class Main {
 
+  static protected WebDriver driver;
 
   static {
-
+    chromedriver().setup();
+    var options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    driver = new ChromeDriver(options);
   }
 
   public static void main(String[] args) {
-
+    driver.get("https://google.com");
+    driver.quit();
   }
 }
